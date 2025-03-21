3f22bf470abf2a82bf2331a7cb0a19b06123c22a
tinashekwande <ttynashy5@gmail.com>
2025-03-21 23:17:33 +0200

    Made my first edit to the react app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hie i am about to make this Cool Portfolio of mine!!
         </p>
         <a
           className="App-link"
